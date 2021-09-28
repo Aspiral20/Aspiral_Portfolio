716e82d498e3061a8889ccfb09f35208a367e717
Victor <palanciucvictor20@gmail.com>
2021-09-28 11:40:31 +0300

    Fixed fon

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ const concatCss = require('gulp-concat-css');
 sass.compiler = require('node-sass');
 
 exports.sass = function () {
-    return src('../src/styles/main.scss')
+    return src('./src/styles/main.scss')
         //Для больше 1 файла используем массив:
         // (['./src/styles/main.scss', './src/styles/additional.scss'])
         //Преобразовали в CSS
@@ -21,10 +21,10 @@ exports.sass = function () {
         //File.min.css
         .pipe(rename({suffix: '.min'}))
         //Сохранить в папку dist
-        .pipe(dest('../dist'));
+        .pipe(dest('./dist'));
 }
 
 exports.sassWatch = function () {
     //Auto-save when choose css code
-    watch('../src/styles/*.scss', series('sass'));
+    watch('./src/styles/*.scss', series('sass'));
 }
